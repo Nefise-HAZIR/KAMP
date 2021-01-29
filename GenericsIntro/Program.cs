0fb4f52dfe17e566bc29abce8778140cdc707f43
NEFİSE HAZIR <2019555034@ogr.cu.edu.tr>
2021-01-29 15:54:45 +0300

    20 ocak ders sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLıst<string> isimler = new MyLıst<string>();
+            isimler.Add("Nefise");
+            List<string> liste = new List<string>();
+            Console.WriteLine(liste.Count);
+
+
+        }
+          
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CatogoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitsInStock = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { UnitsInStock = 6, CatogoryId = 9, ProductName = "kalem", Id = 7, UnıtPrice = 9 };
+            ProductManager productManager=new ProductManager();
+            
+            // int ,double ,bool ..değer tip
+            // diziler,class,... referans tip
+
+            Console.WriteLine(product1.ProductName);
+            productManager.Add(product2);
+           
+           
+
+
+
+
+        }
+    }
+}
