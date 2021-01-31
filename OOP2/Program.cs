d62c63b2e1127ed8b86861948648a12717cc0738
NEFİSE HAZIR <2019555034@ogr.cu.edu.tr>
2021-01-31 15:24:25 +0300

    .

---
+++ OOP2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "1234";
+            musteri1.Adi = "Nefise";
+            musteri1.Soyadi = "Hazır";
+            musteri1.TcNo = "123456";
+            musteri1.Id = 6;
+
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "987654";
+            musteri2.SirketAdi = "GUZEL";
+            musteri2.VergiNo = "1234567";
+
+
+            Musteri musteri3 = new GercekMusteri(); // new =referans no diyebiliriz.
+            Musteri musteri4 = new TuzelMusteri();
+            CustomerManager musteriManager = new CustomerManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+        }   
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IhtiyacKrediManager ıhtiyacKrediManager = new IhtiyacKrediManager();
+            //ıhtiyacKrediManager.Hesapla();
+
+            //TasıtKrediManager tasıtKrediManager = new TasıtKrediManager();
+            //tasıtKrediManager.Hesapla();
+
+            //KonutKrediManager konutKrediManager = new KonutKrediManager();
+            //konutKrediManager.Hesapla();
+
+            IKrediManager ıhtiyacKrediManager = new IhtiyacKrediManager();
+                
+            IKrediManager tasıtKrediManager = new TasıtKrediManager();
+            
+            IKrediManager konutKrediManager = new KonutKrediManager();//Interface referans no yu tutabiliyor ! 
+            
+            ILoggerService databaseloggerService = new DatabaseLoggerService();
+            ILoggerService fileloggerService = new FileLoggerService();
+
+            
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ıhtiyacKrediManager, new DatabaseLoggerService());
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(), new DatabaseLoggerService());
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(), new SmsLoggerService());
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ıhtiyacKrediManager,konutKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
